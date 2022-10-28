2041b9139d7fcd6f92d46acb4d3a0eb693e65d3f
zed <96680664+zuhrull@users.noreply.github.com>
2022-10-29 03:14:14 +0700

    Update Indonesia.js

--- src/pages/Indonesia/Indonesia.js
+++ src/pages/Indonesia/Indonesia.js
@@ -3,23 +3,27 @@ import axios from 'axios';
 import Layout from '../../components/Layout/Layout';
 import Title from '../../components/Layout/Title'
 import Pagination from '../../components/features/Pagination'
+import SidebarRight from '../../components/SidebarRight';
 
 const IndonesiaPage = () => {
 	const [news, setNews] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
 	//pagination
 	const [currentPage, setCurrentPage] = useState(1);
   	const [postsPerPage] = useState(5);
 
 	useEffect(() => {
-		(async () => {
+		(async (searchValue) => {
 			const res = await axios.get(
-				`https://newsapi.org/v2/top-headlines?country=id&pageSize=40&apiKey=84de37a1a2054cc18bafae4caa2694fe`
+				// `https://newsapi.org/v2/top-headlines?q=${searchValue}&country=id&sortBy=publishedAt&apiKey=ba0034918a84489e875d313212d04e19`
+				// `https://newsapi.org/v2/top-headlines?q=${searchValue}&country=id&sortBy=publishedAt&apiKey=84de37a1a2054cc18bafae4caa2694fe`
+				`https://newsapi.org/v2/top-headlines?q=${searchValue}&country=id&sortBy=publishedAt&apiKey=138b25dd620d4a9d82f908a5fb7a9edf`
 			);
 
 			setNews(res.data.articles);
-		})();
-	}, []);
+		})(searchValue);
+	}, [searchValue]);
 
 	// Get current posts
 	const indexOfLastPost = currentPage * postsPerPage;
@@ -30,7 +34,8 @@ const IndonesiaPage = () => {
 	const paginate = pageNumber => setCurrentPage(pageNumber);
 
 	return (
-		
+		<div>
+			<SidebarRight searchValue={searchValue} setSearchValue={setSearchValue} />
 			<Title title="Indonesia">
 				<div className='category-title'>
 					<h2>Indonesia</h2>
@@ -49,6 +54,7 @@ const IndonesiaPage = () => {
 				</div>
 				</section>
 			</Title>
+		</div>
 	);
 };
 
