ef8db56a9ff0152f007d5fc8ed039d96a3d32a3a
zed <96680664+zuhrull@users.noreply.github.com>
2022-10-29 03:01:33 +0700

    Update All.js

--- src/pages/All/All.js
+++ src/pages/All/All.js
@@ -3,25 +3,28 @@ import axios from 'axios';
 import Layout from '../../components/Layout/Layout';
 import Title from '../../components/Layout/Title'
 import Pagination from '../../components/features/Pagination'
+import SidebarRight from '../../components/SidebarRight';
 
 
 const AllPage = () => {
 	const [news, setNews] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
 	//pagination
 	const [currentPage, setCurrentPage] = useState(1);
   	const [postsPerPage] = useState(5);
 
 	useEffect(() => {
-		(async () => {
+		(async (searchValue) => {
 			const res = await axios.get(
-// 				`https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=84de37a1a2054cc18bafae4caa2694fe`
-				`https://newsapi.org/v2/top-headlines?country=id&sortBy=publishedAt&apiKey=ba0034918a84489e875d313212d04e19`
+				// `https://newsapi.org/v2/top-headlines?q=${searchValue}&country=id&sortBy=publishedAt&apiKey=ba0034918a84489e875d313212d04e19`
+				// `https://newsapi.org/v2/top-headlines?q=${searchValue}&country=id&sortBy=publishedAt&apiKey=84de37a1a2054cc18bafae4caa2694fe`
+				`https://newsapi.org/v2/top-headlines?q=${searchValue}&country=us&sortBy=publishedAt&apiKey=138b25dd620d4a9d82f908a5fb7a9edf`
 			);
 
 			setNews(res.data.articles);
-		})();
-	}, []);
+		})(searchValue);
+	}, [searchValue]);
 
 	// Get current posts
 	const indexOfLastPost = currentPage * postsPerPage;
@@ -33,6 +36,8 @@ const AllPage = () => {
 
 
 	return (
+		<div>
+		<SidebarRight searchValue={searchValue} setSearchValue={setSearchValue} />
 		<Title title="All">
 			<div className='category-title'>
 				<h2>All</h2>
@@ -51,6 +56,7 @@ const AllPage = () => {
 				</div>
 			</section>
 		</Title>
+		</div
 	);
 };
 
